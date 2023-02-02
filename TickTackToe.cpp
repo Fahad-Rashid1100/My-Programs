1140bc513cfe9fa503ae0cb5050db360c304977b
Fahad-Rashid1100 <123345898+Fahad-Rashid1100@users.noreply.github.com>
2023-02-02 10:26:59 +0500

    Added Features
    Made changes to displayIntro()
    Created a function chooseChar() to choose 'X' or 'O'
    Created a function chooseGridSize() to set the size of the grid e.g: 3x3, 5x5, etc  <---- (In Progress)

--- TickTackToe.cpp
+++ TickTackToe.cpp
@@ -23,11 +23,56 @@ bool displayIntro()
     return 0;
 }
 
+
+char chooseChar()
+{
+    char choice;
+    repeat:
+    cout << "(Player 1)" << endl;
+    cout << "\'X\'  OR  \'O\'? : ";
+    choice = getchar();
+
+    if(choice != 'X' || choice != 'O' || choice != 'x' || choice != 'o')
+    {
+        cout << "Invalid Input!" << endl;
+        goto repeat;
+    }
+
+    return choice;
+}
+
+void chooseGridSize()
+{
+    int size;
+
+    cout << "What should be the size of the grid?" << endl;
+    cout << "Enter a single integer (e.g : 3 = 3x3) : ";
+    cin >> size;
+
+    char grid[size][size];
+
+    for(int r = 0 ; r < size ; r++)
+    {
+        for(int c = 0 ; c < size ; c++)
+        {
+            
+        }
+    }
+}
+
+
 int main()
 {
     bool intro;
+    char character;
 
     intro = displayIntro();
 
-    cout << endl << intro << endl;
+    if(intro == false)
+    {
+        cout << "\nBye Bye Loser ;p" << endl;
+        return 0;
+    }
+
+    character = chooseChar();
 }
