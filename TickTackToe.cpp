ccacf400517d9086f6b1ffe37d0ac30cec753ffc
Fahad-Rashid1100 <123345898+Fahad-Rashid1100@users.noreply.github.com>
2023-02-20 22:53:11 +0500

    Completed
    Completed the entire game but will add a timer later.

--- TickTackToe.cpp
+++ TickTackToe.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
+#include<fstream>
 
 using namespace std;
 
+int playerX = 0;
+int playerO = 0;
+
+char winner;
+bool draw = false;
+
 bool displayIntro()
 {
+    again2:
     char choice;
-
+    
     cout << "-=+=TICK TACK TOE=+=-\n" << endl;
     cout << "Press ENTER to PLAY!" << endl;
     cout << string(9, ' ') << "OR" << endl;
-    cout << "Enter Q to quit if \nyou're a loser ;p" << endl;
+    cout << "Enter Q or any key to quit if \nyou're a loser ;p" << endl;
     choice = getchar();
 
     if(choice == '\n')
@@ -20,6 +28,7 @@ bool displayIntro()
     {
         return false;
     }
+    
     return 0;
 }
 
@@ -28,11 +37,12 @@ char chooseChar()
 {
     char choice;
     repeat:
+    system("clear");
     cout << "(Player 1)" << endl;
-    cout << "\'X\'  OR  \'O\'? : ";
-    choice = getchar();
+    cout << "\'X\'  OR  \'O\'? (Capital Letter Only) : ";
+    cin >> choice;
 
-    if(choice != char(88) && choice != char(79) && choice != char(120) && choice != char(111))
+    if(choice != 'X'  && choice != 'O')
     {
         cout << "Invalid Input!" << endl;
         goto repeat;
@@ -44,10 +54,20 @@ char chooseChar()
 int getSize()
 {
     int size;
+    
+    system("clear");
 
-    cout << "What should be the size of the grid?" << endl;
+    again:
+    
+    cout << "What should be the size of the grid? (Min Input : 3)" << endl;
     cout << "Enter a single integer (e.g : 3 = 3x3) : ";
     cin >> size;
+
+    if(size < 3)
+    {
+        cout << "The grid size can't be smaller than 3!" << endl;
+        goto again;
+    }
 
     return size;
 }
@@ -66,29 +86,139 @@ char** setGridSize(int size)
         }
     }
 
+    for(int i = 1 ; i <= size ; i++)
+    {
+        grid[size - i][size - 1] = '\0';
+    }
+
+    for(int i = 1 ; i <= size ; i++)
+    {
+        grid[size - 1][size - i] = '\0';
+    }
+
+    grid[size - 1][size - 1] = '\n';
+
     return grid;
 }
 
 
-void gameStart(char** grid, int s, char c)
+bool checkResult(char** grid, int size)
 {
-    int r = 0, col = 0; 
-    char input; 
+    int r = 0, c = 0, cnt = 0;
+
     
-    while (true) {
+    for(int i = 0 ; i < size ; i++)
+    {
+        if(grid[r+i][c+i] == grid[r][c])
+        {
+            cnt++;
+        }
+    }
+    if(cnt == size)
+    {
+        return true;
+    }
+
+    cnt = 0;  
+    for(int i = 0 ; i < size ; i++)
+    {
+        cnt = 0;
+        for(int j = 0 ; j < size ; j++)
+        {
+            if(grid[i][j] == grid[r][c])
+            {
+                cnt++;
+            }
+        }
+        r++;
+        if(cnt == size)
+        {
+            return true;
+        }
+    }
+
+    r = 0;
+    c = 0;
+    cnt = 0;
+    for(int i = size-1 ; i >= 0 ; i--)
+    {
+        if(grid[r][i] == grid[size-1][c])
+        {
+            cnt++;
+        }
+        r++;
+    }
+    if(cnt == size)
+    {
+        return true;
+    }
+
+    r = 0;
+    c = 0;
+    cnt = 0;
+    for(int j = 0 ; j < size ; j++)
+    {
+        cnt = 0;
+        for(int i = 0 ; i < size ; i++)
+        {
+            if(grid[i][j] == grid[r][c])
+            {
+                cnt++;
+            }
+        }
+        c++;
+        if(cnt == size)
+        {
+            return true;
+        }
+    }
+
+     
+    return false;
+}
+
+bool gameStart(char** grid, int s, char c)
+{
+    int r = 0, col = 0, moves = 0; 
+    char input; 
+    bool res;
+    
+    
+    while (draw != true) 
+    {
+
+        cout << "\nUse WASD Keys to move the pointer!" << endl;
+        cout << "NOTE : The Character between [] shows what character will be placed" << endl;
+        cout << "It doesn't show what is already placed." << endl;
         
         for(int i = 0 ; i < s ; i++)
         {
             for(int j  = 0 ; j < s ; j++)
             {
-                if (i == r && j == col) {
-                    cout << "[" << grid[i][j] << "]";
-                } else {
+                if (i == r && j == col) 
+                {
+                    cout << "[" << c << "]";
+                } 
+                else 
+                {
                     cout << " " << grid[i][j] << " ";
                 }
-                if(j != s-1)
+                if(i != s-1)
                 {
-                    cout << "|";
+                    if(j != s-1)
+                    {   
+                        cout << "|";
+                    }
+                }
+                else
+                {
+                    if(j != s-1)
+                    {   
+                        if(i == r && j == col || grid[i][j] != '\0')
+                        cout << "|";
+                        else
+                        cout << " |";
+                    }
                 }
             }
             cout << endl;
@@ -103,30 +233,112 @@ void gameStart(char** grid, int s, char c)
         if (input == 'w' && r > 0) 
         {
             r--;
+            system("clear");
         } 
         else if (input == 's' && r < s-1) 
         {
             r++;
+            system("clear");
         } 
         else if (input == 'a' && col > 0) 
         {
             col--;
+            system("clear");
         } 
         else if (input == 'd' && col < s-1) 
         {
             col++;
+            system("clear");
+        } 
+        else if (input == 'p' && (grid[r][col] != 'X' && grid[r][col] != 'O')) 
+        {
+            system("clear");
+
+            cout << "\a";
+
+            moves++;
+
+            cout << "\'" << c << "\' Placed. Move the Pointer to reveal the placed Character!" << endl;
+
+            grid[r][col] = c;
+
+            if(moves == s*s)
+            {
+                draw = true;
+            }
+
+            res = checkResult(grid, s);
+
+            if(res == true && grid[r][col] != ' ')
+            {
+                winner = c;
+                return true;
+            }
+            
+
+            if(c == 'x' || c == 'X')
+            {
+                c = 'O';
+            }
+            else
+            {
+                c = 'X';
+            }
         } 
         else 
         {
+            system("clear");
             continue;
         }
+        
     }
+
+    return false;
 }
 
 
+bool playAgain()
+{
+    again:
+    int choice;
+
+    cout << "1- New Game\n2- Show Results" << endl;
+    cin >> choice;
+
+    if(choice == 1)
+    {
+        return true;
+    }
+    else if(choice == 2)
+    {
+        return false;
+    }
+    else
+    {
+        cout << "Invalid Input!" << endl;
+        goto again;
+    }
+}
+
+void showResults()
+{
+    system("clear");
+
+    ofstream file;
+
+    cout << "Player X Score : " << playerX << endl;
+    cout << "Player O Score : " << playerO << endl;
+
+    file.open("Scores.txt");
+
+    file << "Player X Score : " << playerX << endl << "Player O Score : " << playerO;
+
+    file.close();
+}
 
 int main()
 {
+    newgame:
     bool intro;
     char character;
     int s;
@@ -146,5 +358,94 @@ int main()
 
     grid = setGridSize(s); 
 
-    gameStart(grid, s, character);
+    bool result = gameStart(grid, s, character);
+
+    if(result == true)
+    {
+        for(int r = 0 ; r < s ; r++)
+        {
+            for(int c  = 0 ; c < s ; c++)
+            {
+                cout << " " << grid[r][c];
+                if(c != s-1)
+                {
+                    cout << " |";
+                }   
+            }
+            cout << endl;
+
+            if(s-1 != r)
+            {
+                cout << string(s*4-1, '-');
+            }
+            cout << endl;
+        }
+
+        if(winner == 'X')
+        {
+            playerX += 100;
+            playerO -= 30;
+        }
+        else
+        {
+            playerO += 100;
+            playerX -= 30;
+        }
+        if(playerX < 0)
+        {
+            playerX = 0;
+        }
+
+        if(playerO < 0)
+        {
+            playerO = 0;
+        }
+
+        cout << "\n \'" << winner << "\' is the Winner!" << endl;
+
+        if(playAgain() == true)
+        {
+            goto newgame;
+        }
+        else
+        {
+            showResults();
+        }
+    }
+    else if(draw == true)
+    {
+        for(int r = 0 ; r < s ; r++)
+        {
+            for(int c  = 0 ; c < s ; c++)
+            {
+                cout << " " << grid[r][c];
+                if(c != s-1)
+                {
+                    cout << " |";
+                }   
+            }
+            cout << endl;
+
+            if(s-1 != r)
+            {
+                cout << string(s*4-1, '-');
+            }
+            cout << endl;
+        }
+
+        playerO += 50;
+        playerX += 50;
+
+        cout << "OH WOW! It's a DRAW!" << endl;
+
+        if(playAgain() == true)
+        {
+            draw = false;
+            goto newgame;
+        }
+        else
+        {
+            showResults();
+        }
+    }
 }
